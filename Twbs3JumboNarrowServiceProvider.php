ba192800d8a43c04065bd579974ccf4f52ef5d16
koojunho <koojh@9cells.com>
2016-02-01 18:04:13 +0900

    세팅

---
+++ Twbs3JumboNarrowServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace NineCells\Assets\Twbs3;
+
+use Illuminate\Support\ServiceProvider;
+
+class Twbs3JumboNarrowServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/resources/assets' => public_path('vendor/ninecells/assets-twbs3-jbtrn-narrow'),
+        ], 'public');
+    }
+
+    public function register()
+    {
+    }
+}
